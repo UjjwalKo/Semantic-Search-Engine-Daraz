6c640c4eb10657ba8d3cf732775cf75a894748b8
Ujjwal Koirala <137780425+UjjwalKo@users.noreply.github.com>
2024-12-07 21:17:49 +0545

    correction

--- daraz_search/build_files.sh
+++ daraz_search/build_files.sh
@@ -1,9 +1,4 @@
 echo "BUILD START"
-which python3
-python3 --version
-which pip3
-pip3 --version
-
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
 echo "BUILD END"
