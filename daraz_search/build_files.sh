fe2f9a742bbeb98d75ba8c3f1e025c8a27a8dea4
Ujjwal Koirala <137780425+UjjwalKo@users.noreply.github.com>
2024-12-07 21:29:28 +0545

    correction

--- daraz_search/build_files.sh
+++ daraz_search/build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-pip3 install -r requirements.txt
-python3 manage.py collectstatic --no-input --clear
+pip install -r requirements.txt
+python manage.py collectstatic --no-input --clear
 echo "BUILD END"
